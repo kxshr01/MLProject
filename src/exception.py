d351a8d35180982c975dd86bfab633deabaf130d
kxshr01 <kaushikrajesh490@gmail.com>
2024-12-24 22:43:30 -0700

    notebook

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+from src.logger import logging
 
 def error_message_detail(error,error_details:sys):
     _,_,exc_tb=error_details.exc_info()
@@ -9,7 +10,7 @@ def error_message_detail(error,error_details:sys):
 
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message = error_message_detail(error_message,error_details=error_detail)
 
 
